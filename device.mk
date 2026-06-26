6df433d1e92951f7238d79ed2ba35c342b312c80
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-26 01:19:38 -0400

    timelm: fastbootd is now enabled by default
    Change-Id: Ieb54140550168f84ba4f067605fd6f7107979c31

--- device.mk
+++ device.mk
@@ -141,10 +141,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
 
-# Fastbootd
-PRODUCT_PACKAGES += \
-    fastbootd
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.lge
